4affbe2764fed54f848690b5e9d1079739992da7
Jason Orendorff <jason.orendorff@gmail.com>
2019-12-15 23:49:12 -0600

    First stab at 2019 day 16 part 1.

---
+++ ad2019/dec16/part1.py
@@ -0,0 +1,40 @@
+
+from lib.advent import *
+
+
+def pattern(w):
+    while True:
+        for e in [0, 1, 0, -1]:
+            for i in range(w):
+                yield e
+
+def phase(arr):
+    return [abs(sum(i * j for i, j in zip(arr, pattern(w)))) % 10
+            for w in range(1, len(arr) + 1)]
+
+def crack(s):
+    assert s.isdigit()
+    return [int(digit) for digit in s]
+
+assert phase(crack('12345678')) == crack('48226158')
+
+assert phase(crack('48226158')) == crack('34040438')
+
+assert fn_exp(phase, 4)(crack('12345678')) == crack('01029498')
+
+
+fft100 = fn_exp(phase, 100)
+
+assert fft100(crack('80871224585914546619083218645595')) == crack('24176176')
+assert fft100(crack('19617804207202209144916044189917')) == crack('73745418')
+assert fft100(crack('69317163492948606335995924319873')) == crack('52432133')
+
+
+def main():
+    s = fft100(crack(puzzle_input()))
+    eight = s[:8]
+    print(''.join(str(digit) for digit in eight))
+
+
+if __name__ == '__main__':
+    main()
