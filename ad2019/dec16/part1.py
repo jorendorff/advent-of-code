ea652733bf7c5f28bbff4af01e2fcaed05c096a8
Jason Orendorff <jason.orendorff@gmail.com>
2019-12-15 23:49:37 -0600

    Part 1. Fixes three bugs.
    Failed to strip the input; and missing two requirements from sloppy
    reading.

--- ad2019/dec16/part1.py
+++ ad2019/dec16/part1.py
@@ -2,11 +2,16 @@
 from lib.advent import *
 
 
-def pattern(w):
+def upattern(w):
     while True:
         for e in [0, 1, 0, -1]:
             for i in range(w):
                 yield e
+
+def pattern(w):
+    it = upattern(w)
+    next(it)
+    return it
 
 def phase(arr):
     return [abs(sum(i * j for i, j in zip(arr, pattern(w)))) % 10
@@ -25,13 +30,13 @@ assert fn_exp(phase, 4)(crack('12345678')) == crack('01029498')
 
 fft100 = fn_exp(phase, 100)
 
-assert fft100(crack('80871224585914546619083218645595')) == crack('24176176')
-assert fft100(crack('19617804207202209144916044189917')) == crack('73745418')
-assert fft100(crack('69317163492948606335995924319873')) == crack('52432133')
+assert fft100(crack('80871224585914546619083218645595'))[:8] == crack('24176176')
+assert fft100(crack('19617804207202209144916044189917'))[:8] == crack('73745418')
+assert fft100(crack('69317163492948606335995924319873'))[:8] == crack('52432133')
 
 
 def main():
-    s = fft100(crack(puzzle_input()))
+    s = fft100(crack(puzzle_input().strip()))
     eight = s[:8]
     print(''.join(str(digit) for digit in eight))
 
