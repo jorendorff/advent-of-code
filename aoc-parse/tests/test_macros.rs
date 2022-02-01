ab3dfb0277f2768a4218608feddf8832479d950b
Jason Orendorff <jason.orendorff@gmail.com>
2022-01-31 18:19:09 -0600

    Tests for alternation and `lines()`

--- aoc-parse/tests/test_macros.rs
+++ aoc-parse/tests/test_macros.rs
@@ -25,4 +25,19 @@ fn test_macros() {
     assert_parse(&p, "hello world");
     assert_parse(&p, "hello strange world");
     assert_parse(&p, "hello strange strange strange strange strange world");
+
+    let p = parser!({"one", "two"});
+    assert_parse(&p, "one");
+    assert_parse(&p, "two");
+    assert_no_parse(&p, "");
+    assert_no_parse(&p, "onetwo");
+    assert_no_parse(&p, "twoone");
+
+    let p = parser!(lines("whee!"));
+    assert_parse(&p, "");
+    assert_parse(&p, "whee!\nwhee!\nwhee!\n");
+    assert_parse(&p, "whee!\n");
+    assert_no_parse(&p, "whee!");
+    assert_no_parse(&p, "\n");
+    assert_no_parse(&p, "whee!\n\n");
 }
