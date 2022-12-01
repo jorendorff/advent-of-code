897fc79f1ab43c50112810f0e5f24b8cc5d09b14
Jason Orendorff <jason.orendorff@gmail.com>
2022-11-30 23:12:33 -0600

    2022 dec 1

---
+++ ad2022/src/day01.rs
@@ -0,0 +1,61 @@
+use aoc_parse::{parser, prelude::*};
+use aoc_runner_derive::*;
+
+/// Parse the puzzle input, which is just a series of numbers, one per line.
+#[aoc_generator(day1, part1, jorendorff)]
+#[aoc_generator(day1, part2, jorendorff)]
+fn parse_input(text: &str) -> anyhow::Result<Vec<Vec<u64>>> {
+    let p = parser!(repeat_sep(repeat_sep(u64, "\n"), "\n\n"));
+    Ok(p.parse(text)?)
+}
+
+#[aoc(day1, part1, jorendorff)]
+fn part_1(lists: &[Vec<u64>]) -> u64 {
+    lists
+        .iter()
+        .map(|list| list.iter().sum::<u64>())
+        .max()
+        .unwrap()
+}
+
+#[aoc(day1, part2, jorendorff)]
+fn part_2(lists: &[Vec<u64>]) -> u64 {
+    let mut totals = lists
+        .iter()
+        .map(|list| list.iter().sum::<u64>())
+        .collect::<Vec<u64>>();
+    totals.sort();
+    totals[totals.len() - 3..totals.len()].iter().sum::<u64>()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const EXAMPLE: &str = "\
+1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000
+";
+
+    #[test]
+    fn test_part_1() {
+        assert_eq!(part_1(&parse_input(EXAMPLE).unwrap()), 24000);
+    }
+
+    //     #[test]
+    //     fn test_part_2() {
+    //         assert_eq!(part_2(&parse_input(EXAMPLE).unwrap()), 5);
+    //     }
+}
