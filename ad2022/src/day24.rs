fde18f18795f46ceab48dd8630bfc5f1d91b8d5b
Jason Orendorff <jorendorff@github.com>
2022-12-23 23:57:30 -0600

    Refactor a bit.

--- ad2022/src/day24.rs
+++ ad2022/src/day24.rs
@@ -7,7 +7,8 @@ use aoc_runner_derive::*;
 enum Dir {
     Up,
     Down,
-    Left, Right,
+    Left,
+    Right,
 }
 use Dir::*;
 
@@ -48,37 +49,31 @@ fn parse_input(text: &str) -> anyhow::Result<Input> {
             row.iter()
                 .copied()
                 .enumerate()
-                .filter_map(move |(x, sign)| {
-                    match sign {
-                        Blizz(dir) => Some(Blizzard {x, y, dir}),
-                        _ => None
-                    }
+                .filter_map(move |(x, sign)| match sign {
+                    Blizz(dir) => Some(Blizzard { x, y, dir }),
+                    _ => None,
                 })
-        }).collect();
+        })
+        .collect();
 
-    let grid: Vec<Vec<bool>> = grid.into_iter()
+    let grid: Vec<Vec<bool>> = grid
+        .into_iter()
         .map(|row| {
             row.into_iter()
-                .map(|sign| {
-                    match sign {
-                        Wall => false,
-                        _ => true,
-                    }
+                .map(|sign| match sign {
+                    Wall => false,
+                    _ => true,
                 })
                 .collect::<Vec<bool>>()
         })
         .collect();
-    
-    Ok((blizzards, grid))
-}
 
-fn first_open<It: IntoIterator<Item=bool>>(it: It) -> usize {
-    it.into_iter().take_while(|v| !v).count()
+    Ok((blizzards, grid))
 }
 
 fn options(width: usize, height: usize, (x, y): (usize, usize)) -> Vec<(usize, usize)> {
     let mut out = vec![(x, y)];
-    
+
     if x > 0 {
         out.push((x - 1, y));
     }
@@ -94,154 +89,120 @@ fn options(width: usize, height: usize, (x, y): (usize, usize)) -> Vec<(usize, u
     out
 }
 
-#[aoc(day24, part1, jorendorff)]
-fn part_1(input: &Input) -> usize {
-    // 288
-    let (blizzards, grid) = input;
+fn move_blizzards(grid: &[Vec<bool>], blizzards: &mut [Blizzard]) {
     let height = grid.len();
     let width = grid[0].len();
 
-    let mut blizzards = blizzards.clone();
-    let x0 = first_open(grid[0].iter().copied());
-    let goal = (first_open(grid[height - 1].iter().copied()), height - 1);
-
-    let mut t = 0;
-
-    // All points we could possibly be at right now
-    let mut points = HashSet::new();
-    points.insert((x0, 0));
-    loop {
-        // move blizzards
-        for b in &mut blizzards {
-            match b.dir {
-                Up => {
-                    if b.y == 0 || !grid[b.y - 1][b.x] {
-                        b.y = (0..height).rev().filter(|&y| grid[y][b.x]).next().unwrap();
-                    } else {
-                        b.y -= 1;
-                    }
-                }
-                Down => {
-                    b.y += 1;
-                    if b.y > height || !grid[b.y][b.x] {
-                        b.y = (0..height).filter(|&y| grid[y][b.x]).next().unwrap();
-                    }
-                }
-                Left => {
-                    b.x -= 1;
-                    if !grid[b.y][b.x] {
-                        b.x = (0..width).rev().filter(|&x| grid[b.y][x]).next().unwrap();
-                    }
-                }
-                Right => {
-                    b.x += 1;
-                    if !grid[b.y][b.x] {
-                        b.x = (0..width).filter(|&x| grid[b.y][x]).next().unwrap();
-                    }
+    for b in blizzards {
+        match b.dir {
+            Up => {
+                if b.y == 0 || !grid[b.y - 1][b.x] {
+                    b.y = (0..height).rev().filter(|&y| grid[y][b.x]).next().unwrap();
+                } else {
+                    b.y -= 1;
                 }
             }
-        }
-
-        // determine possible points
-        let blizzard_locs = blizzards.iter().map(|b| (b.x, b.y)).collect::<HashSet<(usize, usize)>>();
-        let mut after_points = HashSet::new();
-        for point in points {
-            for p in options(width, height, point) {
-                if grid[p.1][p.0] && !blizzard_locs.contains(&p) {
-                    after_points.insert(p);
+            Down => {
+                b.y += 1;
+                if b.y > height || !grid[b.y][b.x] {
+                    b.y = (0..height).filter(|&y| grid[y][b.x]).next().unwrap();
                 }
             }
-        }
-
-        // advance time
-        t += 1;
-        points = after_points;
-
-        // quit if goal achieved
-        assert!(!points.is_empty(), "no safe path");
-        if points.contains(&goal) {
-            return t;
+            Left => {
+                b.x -= 1;
+                if !grid[b.y][b.x] {
+                    b.x = (0..width).rev().filter(|&x| grid[b.y][x]).next().unwrap();
+                }
+            }
+            Right => {
+                b.x += 1;
+                if !grid[b.y][b.x] {
+                    b.x = (0..width).filter(|&x| grid[b.y][x]).next().unwrap();
+                }
+            }
         }
     }
 }
 
-#[aoc(day24, part2, jorendorff)]
-fn part_2(input: &Input) -> usize {
-    // 282
+// determine possible points we can move to
+fn explore(grid: &[Vec<bool>], start: (usize, usize), goal: (usize, usize), blizzards: &[Blizzard], points: HashSet<(usize, usize, usize)>) -> HashSet<(usize, usize, usize)> {
+    let height = grid.len();
+    let width = grid[0].len();
+    let blizzard_locs = blizzards
+        .iter()
+        .map(|b| (b.x, b.y))
+        .collect::<HashSet<(usize, usize)>>();
+    let mut after_points = HashSet::new();
+    for (x, y, stage) in points {
+        for p in options(width, height, (x, y)) {
+            if grid[p.1][p.0] && !blizzard_locs.contains(&p) {
+                let after_stage = if stage == 0 && p == goal {
+                    1
+                } else if stage == 1 && p == start {
+                    2
+                } else {
+                    stage
+                };
+                after_points.insert((p.0, p.1, after_stage));
+            }
+        }
+    }
+    after_points
+}
+
+#[aoc(day24, part1, jorendorff)]
+fn part_1(input: &Input) -> usize {
+    // Rank 288 on the global leaderboard. There are lots of ways to cut
+    // corners here. The problem statement didn't say that the puzzle input
+    // would be a wide-open rectangle (though that does make valid puzzle inputs
+    // much easier to create).
     let (blizzards, grid) = input;
     let height = grid.len();
     let width = grid[0].len();
 
     let mut blizzards = blizzards.clone();
-    let x0 = first_open(grid[0].iter().copied());
+    let x0 = (0..width).filter(|&x| grid[0][x]).next().unwrap();
     let start = (x0, 0);
-    let goal = (first_open(grid[height - 1].iter().copied()), height - 1);
+    let xn = (0..width).filter(|&x| grid[height - 1][x]).next().unwrap();
+    let goal = (xn, height - 1);
 
     let mut t = 0;
-
-    // All points we could possibly be at right now
     let mut points = HashSet::new();
-    points.insert((x0, 0, 0));
-    loop {
-        // move blizzards
-        for b in &mut blizzards {
-            match b.dir {
-                Up => {
-                    if b.y == 0 || !grid[b.y - 1][b.x] {
-                        b.y = (0..height).rev().filter(|&y| grid[y][b.x]).next().unwrap();
-                    } else {
-                        b.y -= 1;
-                    }
-                }
-                Down => {
-                    b.y += 1;
-                    if b.y > height || !grid[b.y][b.x] {
-                        b.y = (0..height).filter(|&y| grid[y][b.x]).next().unwrap();
-                    }
-                }
-                Left => {
-                    b.x -= 1;
-                    if !grid[b.y][b.x] {
-                        b.x = (0..width).rev().filter(|&x| grid[b.y][x]).next().unwrap();
-                    }
-                }
-                Right => {
-                    b.x += 1;
-                    if !grid[b.y][b.x] {
-                        b.x = (0..width).filter(|&x| grid[b.y][x]).next().unwrap();
-                    }
-                }
-            }
-        }
-
-        // determine possible points
-        let blizzard_locs = blizzards.iter().map(|b| (b.x, b.y)).collect::<HashSet<(usize, usize)>>();
-        let mut after_points = HashSet::new();
-        for (x, y, stage) in points {
-            for p in options(width, height, (x, y)) {
-                if grid[p.1][p.0] && !blizzard_locs.contains(&p) {
-                    let after_stage = if stage == 0 && p == goal {
-                        1
-                    } else if stage == 1 && p == start {
-                        2
-                    } else {
-                        stage
-                    };
-                    after_points.insert((p.0, p.1, after_stage));
-                }
-            }
-        }
-
-        // advance time
+    points.insert((start.0, start.1, 0));
+    while !points.contains(&(goal.0, goal.1, 1)) {
+        move_blizzards(grid, &mut blizzards);
+        points = explore(grid, start, goal, &blizzards, points);
         t += 1;
-        points = after_points;
-
-        // quit if goal achieved
         assert!(!points.is_empty(), "no safe path");
-        if points.contains(&(goal.0, goal.1, 2)) {
-            return t;
-        }
     }
+
+    t
+}
+
+#[aoc(day24, part2, jorendorff)]
+fn part_2(input: &Input) -> usize {
+    // Rank 282 on the global leaderboard.
+    let (blizzards, grid) = input;
+    let height = grid.len();
+    let width = grid[0].len();
+
+    let mut blizzards = blizzards.clone();
+    let x0 = (0..width).filter(|&x| grid[0][x]).next().unwrap();
+    let start = (x0, 0);
+    let xn = (0..width).filter(|&x| grid[height - 1][x]).next().unwrap();
+    let goal = (xn, height - 1);
+
+    let mut t = 0;
+    let mut points = HashSet::new();
+    points.insert((start.0, start.1, 0));
+    while !points.contains(&(goal.0, goal.1, 2)) {
+        move_blizzards(grid, &mut blizzards);
+        points = explore(grid, start, goal, &blizzards, points);
+        t += 1;
+        assert!(!points.is_empty(), "no safe path");
+    }
+
+    t
 }
 
 #[cfg(test)]
