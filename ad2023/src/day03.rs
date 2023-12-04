cc8deb41e544017f1c3490453ec9eb809ba0b8b3
Jason Orendorff <jorendorff@github.com>
2023-12-04 06:58:40 -0600

    2023 days 3 and 4

---
+++ ad2023/src/day03.rs
@@ -0,0 +1,116 @@
+use aoc_parse::{parser, prelude::*};
+use aoc_runner_derive::*;
+
+struct PartNumber {
+    row: usize,
+    col: usize,
+    len: usize,
+    value: u64,
+}
+
+struct Engine {
+    numbers: Vec<PartNumber>,
+    symbols: Vec<(usize, usize, char)>,
+}
+
+#[aoc_generator(day3, part1, jorendorff)]
+#[aoc_generator(day3, part2, jorendorff)]
+fn parse_input(text: &str) -> anyhow::Result<Engine> {
+    let p = parser!(lines(any_char+));
+    let input = p.parse(text)?;
+
+    let mut numbers: Vec<PartNumber> = vec![];
+    let mut symbols: Vec<(usize, usize, char)> = vec![];
+    for (r, line) in input.iter().enumerate() {
+        let mut num: Option<PartNumber> = None;
+
+        for (c, chr) in line.iter().copied().enumerate() {
+            if let Some(d) = chr.to_digit(10) {
+                let n = num.get_or_insert_with(|| PartNumber {
+                    row: r,
+                    col: c,
+                    len: 0,
+                    value: 0,
+                });
+                n.len += 1;
+                n.value = 10 * n.value + d as u64;
+            } else {
+                if let Some(record) = num {
+                    numbers.push(record);
+                    num = None;
+                }
+                if chr != '.' {
+                    symbols.push((r, c, chr));
+                }
+            }
+        }
+        if let Some(record) = num {
+            numbers.push(record);
+        }
+    }
+
+    Ok(Engine { numbers, symbols })
+}
+
+impl PartNumber {
+    fn is_adjacent(&self, r: usize, c: usize) -> bool {
+        self.row <= r + 1 && r <= self.row + 1
+            && self.col <= c + 1 && c <= self.col + self.len
+    }
+}
+
+#[aoc(day3, part1, jorendorff)]
+fn part_1(engine: &Engine) -> u64 {
+    engine.numbers
+        .iter()
+        .filter(|num| {
+            engine.symbols
+                .iter()
+                .copied()
+                .any(|(r, c, _)| num.is_adjacent(r, c))
+        })
+        .map(|num| num.value)
+        .sum()
+}
+
+#[aoc(day3, part2, jorendorff)]
+fn part_2(engine: &Engine) -> u64 {
+    engine.symbols.iter().copied().filter_map(|(r, c, chr)| -> Option<u64> {
+        if chr != '*' {
+            return None;
+        }
+        let nums = engine.numbers.iter().filter(|num| num.is_adjacent(r, c)).collect::<Vec<_>>();
+        if nums.len() != 2 {
+            return None;
+        }
+        Some(nums[0].value * nums[1].value)
+    }).sum()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const EXAMPLE: &str = "\
+467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598..
+";
+
+    #[test]
+    fn test_part_1() {
+        assert_eq!(part_1(&parse_input(EXAMPLE).unwrap()), 4361);
+    }
+
+    #[test]
+    fn test_part_2() {
+        assert_eq!(part_2(&parse_input(EXAMPLE).unwrap()), 467835);
+    }
+}
---
+++ ad2023/src/day04.rs
@@ -0,0 +1,81 @@
+use aoc_parse::{parser, prelude::*};
+use aoc_runner_derive::*;
+
+struct Card {
+    winning: Vec<u32>,
+    nums: Vec<u32>,
+}
+
+impl Card {
+    fn hits(&self) -> u64 {
+        self.nums.iter().copied()
+            .filter(|n| self.winning.contains(n))
+            .count() as u64
+    }
+}
+        
+
+#[aoc_generator(day4, part1, jorendorff)]
+#[aoc_generator(day4, part2, jorendorff)]
+fn parse_input(text: &str) -> anyhow::Result<Vec<Card>> {
+    let p = parser!(
+        lines(
+            "Card" ' '+ usize ":" " "+
+                winning:repeat_sep(u32, " "+)
+                " "+ "|" " "+
+                nums:repeat_sep(u32, " "+)
+                => Card { winning, nums }
+        )
+    );
+    Ok(p.parse(text)?)
+}
+
+#[aoc(day4, part1, jorendorff)]
+fn part_1(input: &[Card]) -> u64 {
+    input
+        .iter()
+        .map(|card| {
+            let num_hits = card.hits();
+            if num_hits == 0 {
+                0
+            } else {
+                2_u64.pow(num_hits as u32 - 1)
+            }
+        })
+        .sum()
+}
+
+#[aoc(day4, part2, jorendorff)]
+fn part_2(cards: &[Card]) -> u64 {
+    let mut counts = vec![1; cards.len()];
+    for (i, card) in cards.iter().enumerate() {
+        for j in 0..(card.hits() as usize) {
+            counts[i + 1 + j] += counts[i];
+        }
+    }
+    counts.into_iter().sum()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const EXAMPLE: &str = "\
+Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
+";
+
+    #[test]
+    fn test_part_1() {
+        assert_eq!(part_1(&parse_input(EXAMPLE).unwrap()), 13);
+    }
+
+    #[test]
+    fn test_part_2() {
+        assert_eq!(part_2(&parse_input(EXAMPLE).unwrap()), 30);
+    }
+}
