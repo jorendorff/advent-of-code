256aec463d9932dee9bc60bb0d1b710f34c4a07f
Jason Orendorff <jorendorff@github.com>
2023-12-06 23:35:07 -0600

    Simplification.

--- ad2023/src/day07.rs
+++ ad2023/src/day07.rs
@@ -48,64 +48,44 @@ fn hand_key(hand: &(Vec<i32>, u64)) -> (i32, Vec<i32>) {
 
 #[aoc(day07, part1, jorendorff)]
 fn part_1(input: &Input) -> u64 {
-    // 416
+    // 416 on the global leaderboard
     let mut input = input.clone();
     input.sort_by_key(hand_key);
-    for h in &input {
-        eprintln!("{h:?}, {:?}", hand_key(h).0);
-    }
     input.into_iter().enumerate().map(|(index, (_cards, bid))| bid * (index as u64 + 1)).sum()
-}
-
-fn all_possibles(h: &[i32]) -> Vec<Vec<i32>> {
-    let mut all = vec![vec![]];
-
-    for card in h {
-        let card = *card;
-        if card == -1 {
-            all = all.into_iter().flat_map(|seq| {
-                (2..=14).map(move |c| {
-                    let mut new_seq = seq.clone();
-                    new_seq.push(c);
-                    new_seq
-                })
-            }).collect();
-        } else {
-            for c in &mut all {
-                c.push(card);
-            }
-        }
-    }
-
-    all
 }
 
 fn hand_key_2(hand: &(Vec<i32>, u64)) -> (i32, Vec<i32>) {
     let (nums, _bid) = hand;
-    let ty = all_possibles(nums).into_iter().map(|nums| {
-        let mut hist = std::collections::HashMap::<i32, usize>::new();
-        for card in nums {
-            *hist.entry(card).or_insert(0) += 1;
+    let mut hist = std::collections::HashMap::<i32, usize>::new();
+    let mut jacks = 0usize;
+    for card in nums {
+        if *card == -1 {
+            jacks += 1;
+        } else {
+            *hist.entry(*card).or_insert(0) += 1;
         }
-        let mut freq: Vec<usize> = hist.values().copied().collect();
-        freq.sort_unstable();
-        match *freq {
-            [5] => 6,
-            [1, 4] => 5,
-            [2, 3] => 4,
-            [1, 1, 3] => 3,
-            [1, 2, 2] => 2,
-            [1, 1, 1, 2] => 1,
-            _ => 0,
-        }
-    })
-    .max().unwrap();
+    }
+    let mut freq: Vec<usize> = hist.values().copied().collect();
+    freq.sort_unstable();
+    match freq.last_mut() {
+        Some(item) => *item += jacks,
+        None => freq.push(jacks),
+    }
+    let ty = match *freq {
+        [5] => 6,
+        [1, 4] => 5,
+        [2, 3] => 4,
+        [1, 1, 3] => 3,
+        [1, 2, 2] => 2,
+        [1, 1, 1, 2] => 1,
+        _ => 0,
+    };
     (ty, nums.to_vec())
 }
 
 #[aoc(day07, part2, jorendorff)]
 fn part_2(input: &Input) -> u64 {
-    // 459
+    // 459 on the global leaderboard
     let mut input = input.clone();
     input.sort_by_key(hand_key_2);
     input.into_iter().enumerate().map(|(index, (_cards, bid))| bid * (index as u64 + 1)).sum()
