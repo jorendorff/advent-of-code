5e7219663bed2476e91a890c2d775bb98bb76bed
Jason Orendorff <jorendorff@github.com>
2023-12-19 00:01:01 -0600

    tidy up a bit

--- ad2023/src/day19.rs
+++ ad2023/src/day19.rs
@@ -1,4 +1,5 @@
 use std::collections::HashMap;
+use std::ops::Range;
 
 use aoc_parse::{parser, prelude::*};
 use aoc_runner_derive::*;
@@ -16,136 +17,6 @@ struct RuleSet {
     rules: HashMap<String, Vec<Rule>>,
 }
 
-impl RuleSet {
-    fn accept(&self, part: &Part) -> bool {
-        let mut name = "in";
-        while name != "A" && name != "R" {
-            for rule in self.rules.get(name).unwrap() {
-                match rule {
-                    Rule::Gt(a, b, s) => if part.get(*a) > *b { name = s; break; }
-                    Rule::Lt(a, b, s) => if part.get(*a) < *b { name = s; break; }
-                    Rule::Imm(s) => {name = s; break; }
-                }
-            }
-        }
-        name == "A"
-    }
-
-    fn how_many(&self) -> u64 {
-        // let mut break_points = [vec![1, 4001], vec![1, 4001], vec![1, 4001], vec![1, 4001]];
-        // 
-        // for rules in self.rules.values() {
-        //     for rule in rules {
-        //         match rule {
-        //             Rule::Gt(a, b, _) => break_points[*a].push(*b + 1),
-        //             Rule::Lt(a, b, _) => break_points[*a].push(*b),
-        //             _ => {}
-        //         }
-        //     }
-        // }
-        // 
-        // for v in &mut break_points {
-        //     v.sort();
-        //     v.dedup();
-        // }
-        // 
-        // println!("{} {} {} {}", break_points[0].len(), break_points[1].len(), break_points[2].len(), break_points[3].len());
-        // let mut total = 0;
-        // 
-        // for xr in break_points[0].windows(2) {
-        //     let (x0, x1) = (xr[0], xr[1]);
-        //     let xw = x1 - x0;
-        //     for mr in break_points[1].windows(2) {
-        //         let (m0, m1) = (mr[0], mr[1]);
-        //         let mw = xw * (m1 - m0);
-        //         for ar in break_points[1].windows(2) {
-        //             let (a0, a1) = (ar[0], ar[1]);
-        //             let aw = mw * (a1 - a0);
-        //             for sr in break_points[1].windows(2) {
-        //                 let (s0, s1) = (sr[0], sr[1]);
-        //                 if self.accept(&Part { x: x0, m: m0, a: a0, s: s0 }) {
-        //                     total += aw * (s1 - s0);
-        //                 }
-        //             }
-        //         }
-        //     }
-        // }
-        // 
-        // total
-        self.how_many_match(self.rules.get("in").unwrap(), [(1, 4001); 4])
-    }
-
-    fn workflow(&self, name: &str) -> &[Rule] {
-        self.rules.get(name).unwrap()
-    }
-
-    fn how_many_match_name(&self, name: &str, ranges: [(u64, u64); 4]) -> u64 {
-        match name {
-            "A" => ranges.into_iter().map(|(v0, v1)| v1 - v0).product(),
-            "R" => 0,
-            s => self.how_many_match(self.workflow(s), ranges),
-        }
-    }
-    
-    fn how_many_match(&self, workflow: &[Rule], ranges: [(u64, u64); 4]) -> u64 {
-        match &workflow[0] {
-            Rule::Gt(a, b, s) => {
-                let (a0, a1) = ranges[*a];
-                if a0 > *b {
-                    // all values in range are > b, all match this rule, ignore rest of range
-                    self.how_many_match(self.workflow(s), ranges)
-                } else if a1 <= *b {
-                    // no values in this range are > b, none match this rule, move on
-                    self.how_many_match(&workflow[1..], ranges)
-                } else {
-                    let mut non_matching_range = ranges;
-                    non_matching_range[*a].1 = *b + 1;
-                    let mut matching_range = ranges;
-                    matching_range[*a].0 = *b + 1;
-                    self.how_many_match(&workflow[1..], non_matching_range) + self.how_many_match_name(s, matching_range)
-                }
-            }
-            Rule::Lt(a, b, s) => {
-                let (a0, a1) = ranges[*a];
-                if a1 < *b {
-                    // all values in range are < b, all match this rule, ignore rest of range
-                    self.how_many_match(self.workflow(s), ranges)
-                } else if a0 >= *b {
-                    // no values in this range are < b, none match this rule, move on
-                    self.how_many_match(&workflow[1..], ranges)
-                } else {
-                    let mut matching_range = ranges;
-                    matching_range[*a].1 = *b;
-                    let mut non_matching_range = ranges;
-                    non_matching_range[*a].0 = *b;
-                    self.how_many_match(&workflow[1..], non_matching_range) + self.how_many_match_name(s, matching_range)
-                }
-            }
-            Rule::Imm(s) => self.how_many_match_name(s, ranges),
-        }
-    }
-        
-//      fn combos(&self, symbol: &str, start: usize, mut range: PartRange) -> u64 {
-//          for rule in &self.rules.get(symbol).unwrap()[start..] {
-//              match rule {
-//                  Rule::Gt(a, b, s) => {
-//                      let r = range.get(*a);
-//                      if r.end <= *b {
-//                          // all values are <= b, none match this rule; continue to next rule
-//                      } else if r.start <= *b {
-//                          // some values are <= b, others are > b
-//                          let under = self.combos();
-//                      } else {
-//                          
-//                      }
-//                  }
-//                  Rule::Lt(a, b, s) => panic!(),
-//                  Rule::Imm(s) => return self.combos(s),
-//              }
-//          }
-//  }
-}
-
 #[derive(Clone, Copy, PartialEq, Eq)]
 struct Part {
     x: u64,
@@ -153,29 +24,6 @@ struct Part {
     a: u64,
     s: u64,
 }
-
-
-// #[derive(Clone)]
-// struct PartRange {
-//     x: Range<u64>,
-//     m: Range<u64>,
-//     a: Range<u64>,
-//     s: Range<u64>,
-// }
-
-impl Part {
-    fn rating(&self) -> u64 { self.x+self.m+self.a+self.s}
-
-    fn get(&self, attr: usize) -> u64 {
-        [self.x, self.m, self.a, self.s][attr]
-    }
-}
-
-// impl PartRange {
-//     fn get(&mut self, attr: usize) -> &mut Range<u64> {
-//         [&mut self.x, &mut self.m, &mut self.a, &mut self.s][attr]
-//     }
-// }
 
 #[aoc_generator(day19, part1, jorendorff)]
 #[aoc_generator(day19, part2, jorendorff)]
@@ -198,15 +46,112 @@ fn parse_input(text: &str) -> anyhow::Result<Input> {
     Ok(p.parse(text)?)
 }
 
+impl RuleSet {
+    fn accept(&self, part: &Part) -> bool {
+        let mut name = "in";
+        while name != "A" && name != "R" {
+            for rule in self.workflow(name) {
+                match rule {
+                    Rule::Gt(a, b, s) => {
+                        if part.get(*a) > *b {
+                            name = s;
+                            break;
+                        }
+                    }
+                    Rule::Lt(a, b, s) => {
+                        if part.get(*a) < *b {
+                            name = s;
+                            break;
+                        }
+                    }
+                    Rule::Imm(s) => {
+                        name = s;
+                        break;
+                    }
+                }
+            }
+        }
+        name == "A"
+    }
+
+    fn how_many(&self) -> u64 {
+        self.how_many_match_name("in", [1..4001, 1..4001, 1..4001, 1..4001])
+    }
+
+    fn workflow(&self, name: &str) -> &[Rule] {
+        self.rules.get(name).unwrap()
+    }
+
+    fn how_many_match_name(&self, name: &str, ranges: [Range<u64>; 4]) -> u64 {
+        match name {
+            "A" => ranges.into_iter().map(|r| r.end - r.start).product(),
+            "R" => 0,
+            s => self.how_many_match(self.workflow(s), ranges),
+        }
+    }
+
+    fn how_many_match(&self, workflow: &[Rule], ranges: [Range<u64>; 4]) -> u64 {
+        match &workflow[0] {
+            Rule::Gt(a, b, s) => {
+                let ra = &ranges[*a];
+                if ra.start > *b {
+                    // all values in range are > b, all match this rule, ignore rest of range
+                    self.how_many_match(self.workflow(s), ranges)
+                } else if ra.end <= *b {
+                    // no values in this range are > b, none match this rule, move on
+                    self.how_many_match(&workflow[1..], ranges)
+                } else {
+                    let mut non_matching_range = ranges.clone();
+                    non_matching_range[*a].end = *b + 1;
+                    let mut matching_range = ranges.clone();
+                    matching_range[*a].start = *b + 1;
+                    self.how_many_match(&workflow[1..], non_matching_range)
+                        + self.how_many_match_name(s, matching_range)
+                }
+            }
+            Rule::Lt(a, b, s) => {
+                let ra = &ranges[*a];
+                if ra.end < *b {
+                    // all values in range are < b, all match this rule, ignore rest of range
+                    self.how_many_match(self.workflow(s), ranges)
+                } else if ra.start >= *b {
+                    // no values in this range are < b, none match this rule, move on
+                    self.how_many_match(&workflow[1..], ranges)
+                } else {
+                    let mut matching_range = ranges.clone();
+                    matching_range[*a].end = *b;
+                    let mut non_matching_range = ranges.clone();
+                    non_matching_range[*a].start = *b;
+                    self.how_many_match(&workflow[1..], non_matching_range)
+                        + self.how_many_match_name(s, matching_range)
+                }
+            }
+            Rule::Imm(s) => self.how_many_match_name(s, ranges),
+        }
+    }
+}
+
+impl Part {
+    fn rating(&self) -> u64 {
+        self.x + self.m + self.a + self.s
+    }
+
+    fn get(&self, attr: usize) -> u64 {
+        [self.x, self.m, self.a, self.s][attr]
+    }
+}
+
 #[aoc(day19, part1, jorendorff)]
 fn part_1(input: &Input) -> u64 {
     // #359 on the global leaderboard
     let (ruleset, parts) = input;
 
-    parts.iter().copied().filter(|part| {
-        ruleset.accept(part)
-    }).map(|part| part.rating())
-    .sum()
+    parts
+        .iter()
+        .copied()
+        .filter(|part| ruleset.accept(part))
+        .map(|part| part.rating())
+        .sum()
 }
 
 #[aoc(day19, part2, jorendorff)]
