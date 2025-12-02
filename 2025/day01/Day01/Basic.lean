d4023d53360d826603dff8fe676965da201e3700
Jason Orendorff <jorendorff@github.com>
2025-12-01 18:48:21 -0600

    2025 day 1 part 2

--- 2025/day01/Day01/Basic.lean
+++ 2025/day01/Day01/Basic.lean
@@ -2,6 +2,8 @@ import Std.Internal.Parsec
 
 open Std.Internal.Parsec.String
 
+-- Input parser
+
 def parseLeft : Parser Int := do
   skipChar 'L'
   let n <- digits
@@ -17,11 +19,33 @@ def parseRight : Parser Int := do
 def parser : Parser (Array Int) :=
   (parseLeft <|> parseRight).many
 
-def step (state : Nat × Int) (move : Int) : Nat × Int :=
+-- Part 1
+
+def numbers : Int := 100
+
+def step1 (state : Nat × Int) (move : Int) : Nat × Int :=
   let ⟨count, pos⟩ := state
-  let pos' := (pos + move) % 100
+  let pos' := (pos + move) % numbers
   ⟨count + (if pos' == 0 then 1 else 0), pos'⟩
 
-def solve (input : Array Int) : Nat :=
-  let ⟨count, _⟩ := input.toList.foldl step ⟨0, 50⟩
+def solve1 (input : Array Int) : Nat :=
+  let ⟨count, _⟩ := input.toList.foldl step1 ⟨0, 50⟩
+  count
+
+-- Part 2
+
+-- Return a list of n copies of the given value v.
+def rep {t : Type} (v : t) (n : Nat) : List t :=
+  match n with
+  | 0 => []
+  | Nat.succ n' => List.cons v $ rep v n'
+
+def tickify (move : Int) : List Int :=
+  match move with
+  | Int.ofNat n => rep 1 n
+  | Int.negSucc n => rep (-1) n.succ
+
+def solve2 (input : Array Int) : Nat :=
+  let ticks := input.toList.flatMap tickify
+  let ⟨count, _⟩ := ticks.foldl step1 ⟨0, 50⟩
   count
--- 2025/day01/Main.lean
+++ 2025/day01/Main.lean
@@ -6,10 +6,11 @@ def main (args : List String) : IO Unit := do
   let [filename] := args | IO.println "usage: day01 input.txt"
   let inputStr <- IO.FS.readFile filename
   let result := parser inputStr.mkIterator
-  let action := match result with
-  | ParseResult.error pos err  =>
-      IO.println s!"parse error at {pos}: {err}"
-  | ParseResult.success _ input =>
-      let answer := solve input
-      IO.println s!"{answer}"
-  action
+  match result with
+  | ParseResult.error pos err  => do
+    IO.println s!"parse error at {pos}: {err}"
+  | ParseResult.success _ input => do
+    let answer1 := solve1 input
+    IO.println s!"part 1: {answer1}"
+    let answer2 := solve2 input
+    IO.println s!"part 2: {answer2}"
