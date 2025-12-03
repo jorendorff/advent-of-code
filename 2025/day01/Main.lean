37e6047a2e2b46a1ecce6a2f0381c186070aa080
Jason Orendorff <jorendorff@github.com>
2025-12-03 07:07:03 -0600

    usage

--- 2025/day01/Main.lean
+++ 2025/day01/Main.lean
@@ -3,7 +3,7 @@ import Std.Internal.Parsec
 open Std.Internal.Parsec
 
 def main (args : List String) : IO Unit := do
-  let [filename] := args | IO.println "usage: day01 input.txt"
+  let [filename] := args | IO.println "usage: day01 inputs/example.txt"
   let inputStr <- IO.FS.readFile filename
   let result := parser inputStr.mkIterator
   match result with
--- 2025/day02/Main.lean
+++ 2025/day02/Main.lean
@@ -4,7 +4,7 @@ import Std.Internal.Parsec
 open Std.Internal.Parsec
 
 def main (args : List String) : IO Unit := do
-  let [filename] := args | IO.println "usage: day01 input.txt"
+  let [filename] := args | IO.println "usage: day02 inputs/example.txt"
   let inputStr <- IO.FS.readFile filename
   let result := parser inputStr.mkIterator
   match result with
