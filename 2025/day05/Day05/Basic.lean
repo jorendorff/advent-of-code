271632efe1c3b96c3f19d42e22040d7e5e9f67d9
Jason Orendorff <jorendorff@github.com>
2025-12-05 10:42:33 -0600

    2025 day 5

---
+++ 2025/day05/Day05/Basic.lean
@@ -0,0 +1,56 @@
+import Std.Internal.Parsec
+import Std.Data.HashSet
+
+open Std.Internal.Parsec
+open Std.Internal.Parsec.String
+
+-- # Input parser
+
+structure Range where
+  start : Nat
+  stop : Nat
+deriving Ord
+
+structure Input where
+  fresh : Array Range
+  avail : Array Nat
+
+def range : Parser Range := do
+  let start <- digits
+  skipChar '-'
+  let last <- digits
+  return ⟨start, last + 1⟩
+
+def lines {α : Type} (p : Parser α) : Parser (Array α) :=
+  many do
+    let item <- p
+    skipChar '\n'
+    return item
+
+def parser : Parser Input := do
+  let f <- lines range
+  skipChar '\n'
+  let a <- lines digits
+  return ⟨f, a⟩
+
+-- # Part 1
+
+def fresh? (fresh : Array Range) (id : Nat) : Bool :=
+  fresh.any fun range => range.start <= id && id < range.stop
+
+def solve1 (input : Input) : Nat :=
+  input.avail
+    |> Array.filter (fresh? input.fresh)
+    |> Array.size
+
+-- # Part 2
+
+def solve2 (input : Input) : Nat :=
+  let ⟨_mark, total⟩ := Array.foldl
+    (fun ⟨mark, total⟩ range =>
+      let stop := range.stop
+      let start := min stop (max range.start mark)
+      (max mark stop, total + (stop - start)))
+    (0, 0)
+    input.fresh.qsortOrd
+  total
