5e3d4e603ffc26eecc1f41bff9ab1d20fad58844
Jason Orendorff <jorendorff@github.com>
2025-12-07 04:20:44 -0600

    upgrade day 1 to v.26.0-rc2

--- 2025/day01/Main.lean
+++ 2025/day01/Main.lean
@@ -5,10 +5,10 @@ open Std.Internal.Parsec
 def main (args : List String) : IO Unit := do
   let [filename] := args | IO.println "usage: day01 inputs/example.txt"
   let inputStr <- IO.FS.readFile filename
-  let result := parser inputStr.mkIterator
+  let result := parser ⟨inputStr, inputStr.startValidPos⟩
   match result with
   | ParseResult.error pos err  => do
-    IO.println s!"parse error at {pos}: {err}"
+    IO.println s!"parse error at {pos.snd.offset}: {err}"
   | ParseResult.success _ input => do
     let answer1 := solve1 input
     IO.println s!"part 1: {answer1}"
