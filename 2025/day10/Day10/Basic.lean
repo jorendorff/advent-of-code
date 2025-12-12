0fc99bee7b7a4dcf348fde05576e89f82ad6457a
Jason Orendorff <jorendorff@github.com>
2025-12-11 20:21:48 -0600

    2025 day 10 more dependent types

--- 2025/day10/Day10/Basic.lean
+++ 2025/day10/Day10/Basic.lean
@@ -10,35 +10,45 @@ open Std(HashMap)
 abbrev BitSet := Nat
 
 structure Machine where
-  lights : BitSet
-  wiring : Array (Array Nat)
-  wiringAsBits : Array BitSet
-  joltage : Array Nat
+  numLevels : Nat  -- number of joltage levels
+  lights : Vector Bool numLevels
+  wiring : Array (Array (Fin numLevels))
+  joltage : Vector Nat numLevels
 deriving Repr
 
 abbrev Input := Array Machine
 
 -- Parser combinator for lists with a separator.
-def sepBy1 {t : Type} (sep : Parser Unit) (elem : Parser t) : Parser (Array t) := do
+def commaSep1 {t : Type} (elem : Parser t) : Parser (Array t) := do
   let h <- elem
-  manyCore (attempt $ sep *> elem) #[h]
+  manyCore (skipChar ',' *> elem) #[h]
+
+def parseFin (n : Nat) : Parser (Fin n) := do
+  let x <- digits
+  if h : x < n
+  then
+    have : NeZero n := ⟨Nat.ne_zero_of_lt h⟩
+    return Fin.ofNat n x
+  else fail s!"number out of range: {x}"
 
 def parser : Parser Input := many do
   skipChar '['
   let lights <- many ((skipChar '.' *> return false) <|> (skipChar '#' *> return true))
   skipChar ']'
   skipChar ' '
+  let n := lights.size
   let wiring <- many1 $
-    skipChar '(' *> sepBy1 (skipChar ',') digits <* skipChar ')' <* skipChar ' '
-  let joltage <- skipChar '{' *> sepBy1 (skipChar ',') digits <* skipChar '}'
+    skipChar '(' *> commaSep1 (parseFin n) <* skipChar ')' <* skipChar ' '
+  let joltage <- skipChar '{' *> commaSep1 digits <* skipChar '}'
+  let joltage <-
+    if h : joltage.size = n
+    then pure $ cast (by rw [h]) joltage.toVector
+    else fail s!"number of joltage levels should be = number of display lights"
   skipChar '\n'
-  let lights := lights.foldr (fun bit acc => (acc <<< 1) ||| (if bit then 1 else 0)) 0
-  let wiringAsBits := wiring.map (fun nums =>
-    nums.foldl (fun acc num => acc ||| (1 <<< num)) 0)
   return {
-    lights := lights,
+    numLevels := n,
+    lights := lights.toVector,
     wiring := wiring,
-    wiringAsBits := wiringAsBits,
     joltage := joltage
   }
 
@@ -100,16 +110,22 @@ structure State where
   i : Nat
 deriving BEq, Hashable, Repr
 
-def Machine.successors (m : Machine) (s : State) : Array State :=
-  Array.finRange m.wiringAsBits.size
-    |>.drop s.i
-    |>.map (fun (button : Fin m.wiringAsBits.size) =>
-      {lights := s.lights ^^^ m.wiringAsBits[button], i := button + 1})
-
 def Machine.solve (m : Machine) : Nat :=
+  let lights := m.lights.foldr
+    (fun bit acc => (acc <<< 1) ||| (if bit then 1 else 0))
+    0
+  let wiringAsBits := m.wiring.map (fun nums =>
+    nums.foldl (fun acc num => acc ||| (1 <<< num.toNat)) 0)
+
+  let successors := fun (s : State) =>
+    Array.finRange wiringAsBits.size
+      |>.drop s.i
+      |>.map (fun (button : Fin wiringAsBits.size) =>
+        {lights := s.lights ^^^ wiringAsBits[button], i := button + 1 : State})
+
   let result := search
-    m.successors
-    (fun state => state.lights == m.lights)
+    successors
+    (fun state => state.lights == lights)
     { lights := 0, i := 0 }
   match result with
   | none => panic! "path not found for machine {repr m}"
@@ -120,29 +136,52 @@ def solve1 (input : Input) : Nat :=
 
 -- # Part 2
 
-def Machine.joltageSuccessors (m : Machine) (levels : Array Nat)
-  : Array (Array Nat)
-:= Array.finRange m.wiring.size
-  |>.filterMap (fun (button : Fin m.wiring.size) =>
-    let w := m.wiring[button]
-    if w.all (fun i => levels[i]! < m.joltage[i]!)
+
+structure State2 (m : Machine) where
+  levels : Vector Nat m.numLevels
+  -- next button to consider
+  i : Fin m.wiring.size
+deriving BEq, Repr
+
+-- can't derive Hashable because Vectors aren't hashable for some reason
+instance {m : Machine} : Hashable (State2 m) where
+  hash s := mixHash (hash s.levels.toArray) (hash s.i)
+
+def Machine.joltageSuccessors (m : Machine) (s : State2 m) : Array (State2 m) :=
+  --dbg_trace "looking for paths forward from {repr s}"
+  let options := Array.emptyWithCapacity 2
+  let options :=
+    let w := m.wiring[s.i]
+    let levels' := s.levels |> w.foldl (fun levels i =>
+      let x := levels[i]!
+      levels.set! i x.succ)
+    if w.all (fun i => levels'[i]! <= m.joltage[i]!)
+    then options.push {s with levels := levels'}
+    else options
+  let options :=
+    if h1 : s.i.succ < m.wiring.size
     then
-      let levels' := levels |> w.foldl (fun levels i =>
-        let x := levels[i]!
-        levels.set! i x.succ)
-      some levels'
-    else none)
+      have : NeZero m.wiring.size := by constructor; exact Nat.ne_zero_of_lt h1
+      options.push {s with i := s.i + 1}
+    else options
+  --dbg_trace "  found: {repr options}"
+  options
 
 def Machine.solveJoltage (m : Machine) : Nat :=
-  let result := search
-    m.joltageSuccessors
-    (fun levels => levels == m.joltage)
-    (Array.replicate m.joltage.size 0)
-  match result with
-  | none => panic! s!"path not found for machine {repr m}"
-  | some path =>
-    dbg_trace "{path.length}"
-    path.length
+  if h : m.wiring.size = 0
+  then
+    if m.joltage.all (· = 0) then 0 else panic! "no buttons to mash"
+  else
+    have : NeZero m.wiring.size := NeZero.mk h
+    let result := search
+      m.joltageSuccessors
+      (fun s => s.levels == m.joltage)
+      {levels := Vector.replicate m.numLevels 0, i := 0}
+    match result with
+    | none => panic! s!"path not found for machine {repr m}"
+    | some path =>
+      dbg_trace "{path.length}"
+      path.length
 
 def solve2 (input : Input) : Nat :=
   input.map Machine.solveJoltage |>.sum
